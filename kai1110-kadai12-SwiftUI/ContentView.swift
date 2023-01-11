3d364500b6e0e4536a151c3d0bc1da5e7408d35c
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-01-12 01:59:32 +0900

    commit

---
+++ kai1110-kadai12-SwiftUI/ContentView.swift
@@ -0,0 +1,58 @@
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("taxRate") var taxRate = ""
+    @State var withoutTax = ""
+    @State var includingTax = 0
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("税抜き金額")
+                TextFieldView(value: $withoutTax)
+                Text("円")
+            }
+            HStack {
+                Text("消費税率")
+                TextFieldView(value: $taxRate)
+                Text("%")
+            }
+            Button(action: {
+                includingTax = calclateTax(price: withoutTax, taxRate: taxRate)
+            }) {
+                Text("計算")
+            }
+            HStack {
+                Text("税込金額")
+                Text(String(includingTax))
+                    .padding()
+                Text("円")
+            }
+        }
+    }
+    func parseNumber(number: String) -> Int {
+        Int(number) ?? 0
+    }
+    func calclateTax(price: String, taxRate: String) -> Int {
+        let tax = parseNumber(number: price) * parseNumber(number: taxRate) / 100
+        return parseNumber(number: price) + tax
+    }
+}
+
+struct TextFieldView: View {
+    @Binding var value: String
+    
+    var body: some View {
+        TextField("", text: $value)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .frame(width: 150)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kai1110-kadai12-SwiftUI/kai1110_kadai12_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  kai1110_kadai12_SwiftUIApp.swift
+//  kai1110-kadai12-SwiftUI
+//
+//  Created by 渡邊魁優 on 2023/01/08.
+//
+
+import SwiftUI
+
+@main
+struct kai1110_kadai12_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
